f03ff6c8de89c5914eb137e39ee0bcc0ee884c88
Amit Srivastava <amit.srivastava4all@gmail.com>
2022-07-31 14:04:57 +0530

    Create Sudoku.java

---
+++ Sudoku.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    private boolean canIPlace(char[][] board, int currentCellValue){
+        // In Same Row
+        // In Same Col
+        // Sub Matrix % 3
+        // true / false
+    }
+    public void solveSudoku(char[][] board) {
+        // 
+        for(int col = 0; col<board[row].length; col++){}
+        if(canIPlace(board, board[row][col])){
+            
+}
+    }
+}
